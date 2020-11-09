97d07277199c927ab48583733671209755ae2e49
kalyangoud145 <kalyangoud145@gmail.com>
2020-11-09 14:00:29 +0530

    [Kalyan] implemented frequency of word count in paragraph operation

--- Program.cs
+++ Program.cs
@@ -5,19 +5,26 @@ namespace HashMap
 {
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            //hash takes int and string as parameters of size 6
-            MyMapNode<int,string> hash = new MyMapNode<int, string>(6);
-            //Adds details to hash
-            hash.Add(0, "To");
-            hash.Add(1, "be");
-            hash.Add(2, "or");
-            hash.Add(3, "not");
-            hash.Add(4, "to");
-            hash.Add(5, "be");
             Function function = new Function();
-            //Calls frequencyOfWord method 
+            string paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            //Split converts paragraph  into array of sub strings
+            string[] para = paragraph.Split(" ");
+            MyMapNode<int, string> hash = new MyMapNode<int, string>(para.Length);
+            int key = 0;
+            //Loop iterates on paragraph and adds key and value of each substing
+            //To hash
+            foreach (string word in para)
+            {
+                hash.Add(key, word);
+                key++;
+            }
+            //Calls Frequency of word method to print word count
             function.FrequencyOfWord(hash);
         }
     }
